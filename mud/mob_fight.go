d7f5a0b282af79007eb8e6c95c0335456b460bc5
Brian Seitel <brian@guidebox.com>
2017-03-20 00:10:34 -0700

    reorganize file

--- mud/mob_fight.go
+++ mud/mob_fight.go
@@ -6,6 +6,60 @@ import (
 	"github.com/brianseitel/oasis-mud/helpers"
 )
 
+func (m *mob) attack(target *mob, f *fight) {
+	if target.Status != dead {
+		damage := m.damage(target)
+		target.takeDamage(damage)
+		target.notify(fmt.Sprintf("%s attacks you for %d damagel!%s", m.Name, damage, helpers.Newline))
+		m.notify(fmt.Sprintf("You strike %s for %d damagel!%s", target.Name, damage, helpers.Newline))
+
+		if target.Status == dead {
+			m.notify(fmt.Sprintf("You have KILLED %s to death!!%s", target.Name, helpers.Newline))
+			m.Status = standing
+
+			exp := xpCompute(m, target)
+			m.Exp += exp
+			m.notify(fmt.Sprintf("You gain %d experience points!%s", exp, helpers.Newline))
+			m.checkLevelUp()
+
+			// whisk it away
+			target.die()
+			m.Room.removeMob(target)
+			target.Room.removeMob(target)
+			m.statusBar()
+			return
+		}
+		m.Status = fighting
+		target.Status = fighting
+	}
+}
+
+func (m *mob) damage(victim *mob) int {
+	if m != victim {
+		if victim.parry(m) {
+			return 0
+		}
+
+		if victim.dodge(m) {
+			return 0
+		}
+	}
+	return m.oneHit(victim)
+}
+
+func (m *mob) damroll() int {
+	return m.Strength
+}
+
+func (m *mob) die() {
+	// drop corpse in room
+	corpse := &item{itemType: "corpse", Name: "A corpse of " + m.Name, Identifiers: "corpse," + m.Identifiers, Decays: decays, TTL: 1}
+	m.Room.Items = append(m.Room.Items, corpse)
+
+	// whisk them away to Nowhere
+	m.Room = getRoom(0)
+}
+
 func (m *mob) dodge(attacker *mob) bool {
 	var chance int
 
@@ -30,6 +84,39 @@ func (m *mob) dodge(attacker *mob) bool {
 	attacker.notify(fmt.Sprintf("%s dodges your attack.", attacker.Name))
 
 	return true
+}
+
+func (m *mob) oneHit(victim *mob) int {
+	var dam int
+	if m.isNPC() {
+		dam = dice().Intn(int(m.Level*3/2)) + int(m.Level/2)
+		if m.equippedItem("wield") != nil {
+			dam += int(dam / 2)
+		}
+	} else {
+		if m.equippedItem("wield") != nil {
+			wield := m.equippedItem("wield")
+			dam = dice().Intn(int(wield.Max)) + wield.Min
+		} else {
+			dam = dice().Intn(4) + 1
+		}
+	}
+
+	dam += m.damroll()
+
+	if !m.isNPC() && int(m.skill("enhanced_damage").Level) > 0 {
+		dam += int(dam * int(m.skill("enhanced_damage").Level) / 100)
+	}
+
+	if victim.Status < sitting {
+		dam *= 2
+	}
+
+	if dam <= 0 {
+		dam = 1
+	}
+
+	return dam
 }
 
 func (m *mob) parry(attacker *mob) bool {
@@ -62,97 +149,10 @@ func (m *mob) parry(attacker *mob) bool {
 	return true
 }
 
-func (m *mob) die() {
-	// drop corpse in room
-	corpse := &item{itemType: "corpse", Name: "A corpse of " + m.Name, Identifiers: "corpse," + m.Identifiers, Decays: decays, TTL: 1}
-	m.Room.Items = append(m.Room.Items, corpse)
-
-	// whisk them away to Nowhere
-	m.Room = getRoom(0)
-}
-
-func (m *mob) damroll() int {
-	return m.Strength
-}
-
-func (m *mob) damage(victim *mob) int {
-	if m != victim {
-		if victim.parry(m) {
-			return 0
-		}
-
-		if victim.dodge(m) {
-			return 0
-		}
-	}
-	return m.oneHit(victim)
-}
-
-func (m *mob) oneHit(victim *mob) int {
-	var dam int
-	if m.isNPC() {
-		dam = dice().Intn(int(m.Level*3/2)) + int(m.Level/2)
-		if m.equippedItem("wield") != nil {
-			dam += int(dam / 2)
-		}
-	} else {
-		if m.equippedItem("wield") != nil {
-			wield := m.equippedItem("wield")
-			dam = dice().Intn(int(wield.Max)) + wield.Min
-		} else {
-			dam = dice().Intn(4) + 1
-		}
-	}
-
-	dam += m.damroll()
-
-	// if m.Playable == true && m.getSkill(sEnhancedDamage) > 0 {
-	// 	dam += int(dam * m.getSkill(sEnhancedDamage) / 100)
-	// }
-
-	if victim.Status < sitting {
-		dam *= 2
-	}
-
-	if dam <= 0 {
-		dam = 1
-	}
-
-	return dam
-}
-
 func (m *mob) takeDamage(damage int) {
 	m.Hitpoints -= damage
 	if m.Hitpoints < 0 {
 		m.Status = dead
 		m.notify(helpers.Red + "You are DEAD!!!" + helpers.Reset)
-	}
-}
-
-func (m *mob) attack(target *mob, f *fight) {
-	if target.Status != dead {
-		damage := m.damage(target)
-		target.takeDamage(damage)
-		target.notify(fmt.Sprintf("%s attacks you for %d damagel!%s", m.Name, damage, helpers.Newline))
-		m.notify(fmt.Sprintf("You strike %s for %d damagel!%s", target.Name, damage, helpers.Newline))
-
-		if target.Status == dead {
-			m.notify(fmt.Sprintf("You have KILLED %s to death!!%s", target.Name, helpers.Newline))
-			m.Status = standing
-
-			exp := xpCompute(m, target)
-			m.Exp += exp
-			m.notify(fmt.Sprintf("You gain %d experience points!%s", exp, helpers.Newline))
-			m.checkLevelUp()
-
-			// whisk it away
-			target.die()
-			m.Room.removeMob(target)
-			target.Room.removeMob(target)
-			m.statusBar()
-			return
-		}
-		m.Status = fighting
-		target.Status = fighting
 	}
 }
